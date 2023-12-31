a8b7855c04d3157154cf82892076566c269a2fd7
Ziad Hamdi Saad <130312784+ziad768@users.noreply.github.com>
2023-12-31 23:00:43 +0200

    Update Store.jsx

--- src/Redux/Store.jsx
+++ src/Redux/Store.jsx
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userSlice from "./Slice/userSlice";
 import serviceSlice from "./Slice/serviceSlice";
-import blogSlice from "./slice/blogSlice.jsx";
+import blogSlice from "./Slice/blogSlice";
 import papersSlice from "./Slice/papersSlice";
 const store = configureStore({
   reducer: {
