fc9cbf0cbbdaa0b7a7b46b619bc77eecc20d349c
Ziad Hamdi Saad <130312784+ziad768@users.noreply.github.com>
2024-04-25 00:37:17 +0200

    Update DetailsBlogs.jsx

--- src/Pages/Blogs/DetailsBlogs.jsx
+++ src/Pages/Blogs/DetailsBlogs.jsx
@@ -1,5 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
-import { fetchBlogById } from "../../redux/slice/blogSlice";
+import { fetchBlogById } from "../../redux/Slice/blogSlice";
 import { useParams } from "react-router-dom";
 import { useEffect } from "react";
 
